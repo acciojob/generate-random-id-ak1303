4349845767917275f340b9064abec36a4216243f
ak1303 <94529241+ak1303@users.noreply.github.com>
2024-02-27 00:30:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
   // write your code here
+	let str = 'abcdefghijklmnopqurstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let ans='';
+  for(let i=0;i<l;i++){
+    let randomIndex = Math.floor(Math.random()*62);
+    ans+=str[randomIndex];
+  }
+  return ans; 
 }
 
 // Do not change the code below.
